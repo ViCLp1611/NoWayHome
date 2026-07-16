ecbb4a03ea467c92c3b13e947eff516a61901b87
Vicente Luis Perez <viclp1611@gmail.com>
2026-07-15 20:03:44 -0600

    fix: allow admin email CORS header

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ app.use(
       return callback(new Error('Origen no permitido por CORS.'))
     },
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'x-admin-role'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-role', 'x-admin-email'],
   })
 )
 app.use(express.json({ limit: '1mb' }))
